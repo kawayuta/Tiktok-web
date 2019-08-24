698b9b6e1c5e3df829e675b6bd96ae58bef7ee44
kawayuta <k216q013@stu.kyoto-seika.ac.jp>
2019-08-24 13:59:47 +0900

    get trending_data from official page

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Sample
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.active_record.default_timezone = :local
+    config.active_job.queue_adapter =：delayed_job
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
