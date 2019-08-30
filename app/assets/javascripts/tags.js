d559892e00692a27e48cdc6f8c96c3630c219850
kawayuta <k216q013@stu.kyoto-seika.ac.jp>
2019-08-30 22:20:53 +0900

    sp ui fix

--- app/assets/javascripts/tags.js
+++ app/assets/javascripts/tags.js
@@ -10,10 +10,13 @@ var video = $('#video');
       $(".video_index_" + i).data('video-source') +'">' +
       '</video>' +
       '</div>');
-    $(".video_index_" + i).get(0).load();
-    $(".video_index_" + i).prop('muted', false);
-    $(".video_index_" + i).get(0).play();
-    $(".video_index_" + i).get(0).pause();
+    (function () {
+      $(".video_index_" + i).load();
+      $(".video_index_" + i).prop('muted', false);
+      $(".video_index_" + i).play();
+      $(".video_index_" + i).pause();
+    });
+
 
   }
 video.attr('src', $(".video_index_0").data('video-source'));
