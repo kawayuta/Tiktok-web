d7024437e6661297b44a442cb8ba2833ec5064bf
kawayuta <k216q013@stu.kyoto-seika.ac.jp>
2019-09-02 19:12:38 +0900

    sp ui fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -37,6 +37,16 @@ namespace :deploy do
     end
   end
 
+  task :task_tag_update do
+    on roles(:db) do |host|
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, 'task_database:get_tag_data'
+        end
+      end
+    end
+  end
+
   task :task_input_trending do
     on roles(:db) do |host|
       within current_path do
