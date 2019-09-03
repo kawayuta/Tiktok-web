b41e616c6700f2e426b7298b25dd6d28e90ccad5
kawayuta <k216q013@stu.kyoto-seika.ac.jp>
2019-09-03 23:20:58 +0900

    sp ui fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,5 +5,12 @@ class WelcomeController < ApplicationController
 
     @videos = Video.eager_load(:user).where(video_trending: true).shuffle.take(30)
     @users = @videos.pluck(:user_id).uniq
+
+    @videos_trend_all = Video.eager_load(:user).where(video_trending: true)
+    @videos_trend_rank = @videos_trend_all.sort_by {|array| Integer(array.video_interaction_count)}.reverse
+
+    @videos_all = Video.eager_load(:user)
+    @videos_all_rank = @videos_all.sort_by {|array| Integer(array.video_interaction_count)}.reverse
+
   end
 end
