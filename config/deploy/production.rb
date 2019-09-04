4fd5af583133e52217b878bba64f3f968f1554c8
kawayuta <k216q013@stu.kyoto-seika.ac.jp>
2019-09-04 18:11:51 +0900

    fi

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -90,7 +90,7 @@ namespace :deploy do
     on roles(:app) do |host|
       within current_path do
         with rails_env: fetch(:rails_env) do
-          execute :rake, 'sidekiq'
+          execute :bundle, :exec,　"sidekiq -e production"
         end
       end
     end
