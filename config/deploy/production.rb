78f058341e689abf262eaa5297d441160759ddab
kawayuta <k216q013@stu.kyoto-seika.ac.jp>
2019-09-04 16:47:56 +0900

    fi

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -19,8 +19,7 @@ set :keep_releases, 5
 set :deploy_via, :remote_cache
 
 set :log_level, :debug
-set :pty, true
-
+set :pty, false
 
 set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs,  %w{bin log tmp/pids tmp/sockets tmp/cache vender/bundle}
