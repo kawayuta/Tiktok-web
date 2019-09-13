fb887d3213d5a668a485f0cfb4beef75451bb2fe
kawayuta <k216q013@stu.kyoto-seika.ac.jp>
2019-09-13 13:41:17 +0900

    sp ui fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require activestorage
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
-//= require chartkick
+//= require rails-ujs
+//= require activestorage
 //= require Chart.bundle
+//= require chartkick
 //= require_tree .
